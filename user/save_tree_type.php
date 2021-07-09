145626f6f8ea46149fe7ba837a18aeacd1757cfe
Geofreykim <geofkim98@gmail.com>
2021-07-09 23:07:02 +0300

    Nema work

---
+++ user/save_tree_type.php
@@ -0,0 +1,15 @@
+<<?php 
+require('nema.php');
+
+ if(!isset($_POST['tree_type_name'])){
+ 	echo "Name is not set";
+ 	exit();
+ }
+
+$t_name = $_POST['tree_type_name'];
+
+$sql_connection->query("INSERT INTO tree_types(TYPE_NAME)VALUES('$t_name')");
+
+header("Location:tree_types.php");
+
+ 
